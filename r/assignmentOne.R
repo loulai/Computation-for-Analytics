590703c18bffea9a92e8ce629284b8c7f7e80d50
Louise Lai <louiselai88@gmail.com>
2018-07-18 17:10:00 -0700

    added and refactored

--- r/assignmentOne.R
+++ r/assignmentOne.R
@@ -194,7 +194,7 @@ abline(b0, b1, col="red")
 get_residuals <- function(x_list, y_list, predictedY){
   residuals = c()
   for(i in 1:length(x_list)){
-    residuals[i] = y_list[i] - predictedY[i]
+    residuals[i] = (y_list[i] - predictedY[i])**2
   }
   return(residuals)
 }
@@ -210,15 +210,6 @@ set.seed(999)
 x_2 <- rnorm(100000, -100, 100)
 y_2 <- rexp(100000, rate = 0.5)
 
-get_b1 <- function(x_list, y_list)
-get_b0 <- function(x_list, y_list)
-get_predicted_y_values <- function(x_list, b0, b1) # might break due to variable overlap b0 b1
-get_SSE <- function(x_list, y_list, predictedY)
-get_SSTo <- function(x_list, y_list)
-get_SSR <- function(SSTo, SSE)
-get_rSquared <- function(SSR, SSTo)
-get_residuals <- function(x_list, y_list, predictedY)
-  
 b1_2 <- get_b1(x_2, y_2) # -3.205058e-05
 b0_2 <- get_b0(x_2, y_2, b1_2) # 1.999032
 predictedY_2 <- get_predicted_y_values(x_2, b0_2, b1_2)
@@ -228,6 +219,32 @@ SSR_2 <- get_SSR(SSTo_2, SSE_2) # 1.0323
 rSquared_2 <- get_rSquared(SSR_2, SSTo_2) # 2.589701e-06
 residuals_2 <- get_residuals(x_2, y_2, predictedY_2) # list
 
+plot(x_2, y_2, main="The Second Beautiful Scatterplot", xlab="x values", ylab="y values", pch=20, cex=0.2)
+abline(b0_2, b1_2, col="red")
+
+plot(x_2, residuals_2, main="Residuals", xlab="x values", ylab="residuals (ei)", pch=20, cex=0.2)
+abline(0,0, col="red")
+
+#### 
+set.seed(543)
+x_3 <- rnorm(100000, -100, 100)
+y_3 <- rnorm(100000, -100, 100)
+
+b1_3 <- get_b1(x_3, y_3) # -3.205058e-05
+b0_3 <- get_b0(x_3, y_3, b1_3) # 1.999032
+predictedY_3 <- get_predicted_y_values(x_3, b0_3, b1_3)
+SSE_3 <- get_SSE(x_3, y_3, predictedY_3) # 398,628.3
+SSTo_3 <- get_SSTo(x_3, y_3) # 398,629.4
+SSR_3 <- get_SSR(SSTo_3, SSE_3) # 1.0323
+rSquared_3 <- get_rSquared(SSR_3, SSTo_3) # 2.589701e-06
+residuals_3 <- get_residuals(x_3, y_3, predictedY_3) # list
+
+plot(x_3, y_3, main="The Third Beautiful Scatterplot", xlab="x values", ylab="y values", pch=20, cex=0.2)
+abline(b0_3, b1_3, col="red")
+
+plot(x_3, residuals_3, main="Residuals", xlab="x values", ylab="residuals (ei)", pch=20, cex=0.2)
+abline(0,0, col="red")
+
 
 # Proof through R's package
 rFunction_1 <- lm(y_2 ~ x_2, data.frame(x_2, y_2)) # intercept = 1.999e+00, slope = -3.205e-05
@@ -235,4 +252,15 @@ summary(rFunction_1)
 plot(resid(rFunction) ~ fitted(rFunction))
 plot(x_2, y_2)
 
+
+# just a storage of functions
+get_b1 <- function(x_list, y_list)
+  get_b0 <- function(x_list, y_list)
+    get_predicted_y_values <- function(x_list, b0, b1) # might break due to variable overlap b0 b1
+      get_SSE <- function(x_list, y_list, predictedY)
+        get_SSTo <- function(x_list, y_list)
+          get_SSR <- function(SSTo, SSE)
+            get_rSquared <- function(SSR, SSTo)
+              get_residuals <- function(x_list, y_list, predictedY)
+                
   
